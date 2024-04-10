2f8d8d723e27724449fb18d68b7e503c6c4d6540
Yejin.Shin <mingmi_27@naver.com>
2024-04-11 00:41:53 +0900

    feat : progressbar create

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -13,12 +13,28 @@ class StopWatchScreen extends StatefulWidget {
 class _StopWatchScreenState extends State<StopWatchScreen> {
   Timer? _timer;
 
-  int _time = 5800;
+  int _time = 57600;
+  int _currentTime = 100;
+  double _progress = 0;
+  double remainTime() {
+    return _currentTime / _time;
+  }
+
   bool _isRunning = false;
 
-  List<int> _lapTimes = [];
+  String timeFormat() {
+    int hours = (_currentTime ~/ 3600).toInt();
+    int minutes = (_currentTime ~/ 60 % 60).toInt();
+    int seconds = (_currentTime % 60).toInt();
 
-  //var _lapTimes2 = List.generate(_lapTimes.length, (i) => Text(i));
+    String hoursStr = hours.toString().padLeft(2, '0');
+    String minutesStr = minutes.toString().padLeft(2, '0');
+    String secondsStr = seconds.toString().padLeft(2, '0');
+
+    String timeString = '$hoursStr:$minutesStr:$secondsStr';
+
+    return timeString;
+  }
 
   void _clickButton() {
     _isRunning = !_isRunning;
@@ -31,9 +47,9 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
   }
 
   void _start() {
-    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
-        _time++;
+        _currentTime++;
       });
     });
   }
@@ -42,64 +58,18 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
     _timer?.cancel();
   }
 
-  void _reset() {
-    _isRunning = false;
-    _timer?.cancel();
-    _lapTimes.clear();
-    _time = 0;
-  }
-
-  void _recordLapTime(int _time) {
-    _lapTimes.insert(0, _time);
-  }
-
   @override
   void dispose() {
     _timer?.cancel();
     super.dispose();
   }
 
-  Widget formatting() {
-    return ListView(
-      children: _lapTimes
-          .map(
-            (e) => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                    '${List.from(_lapTimes.reversed).indexOf(e) + 1}'
-                        .padLeft(2, '0'),
-                    style: TextStyle(fontSize: 17)),
-                Text('${toMin(e)}:${toSec(e)}.${toMillisec(e)}',
-                    style: TextStyle(fontSize: 17))
-              ],
-            ),
-          )
-          .toList(),
-    );
-  }
-
-  String toMin(int time) {
-    return '${(time ~/ 6000).remainder(60)}'.padLeft(2, '0');
-  }
-
-  String toSec(int time) {
-    return '${(time ~/ 100).remainder(60)}'.padLeft(2, '0');
-  }
-
-  String toMillisec(int time) {
-    return '${time % 100}'.padLeft(2, '0');
-  }
-
   @override
   Widget build(BuildContext context) {
-    String min = '${(_time ~/ 6000).remainder(60)}'.padLeft(2, '0');
-    String sec = '${(_time ~/ 100).remainder(60)}'.padLeft(2, '0');
-    String millisec = '${(_time % 100)}'.padLeft(2, '0');
     return Scaffold(
-      backgroundColor: Color(0xfffffbff),
+      backgroundColor: const Color(0xfffffbff),
       appBar: AppBar(
-        title: const Text('StopWatch'),
+        title: const Text('단식 추적기'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -107,52 +77,42 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
           const SizedBox(
             height: 30,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                min,
-                style: TextStyle(fontSize: 50),
-              ),
-              const Text(
-                ':',
-                style: TextStyle(fontSize: 50),
-              ),
-              Text(
-                sec,
-                style: TextStyle(fontSize: 50),
-              ),
-              const Text(
-                '.',
-                style: TextStyle(fontSize: 50),
-              ),
-              Text(
-                millisec,
-                style: TextStyle(fontSize: 50),
-              ),
-            ],
+          CircularProgress(
+            progress: _progress,
           ),
-          SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton(
+          const SizedBox(height: 40),
+          Center(
+            child: Container(
+              width: 260,
+              child: FloatingActionButton(
                 onPressed: () {
                   setState(() {
                     _clickButton();
                   });
                 },
                 child: _isRunning
-                    ? const Icon(Icons.pause)
-                    : const Icon(Icons.play_arrow),
+                    ? const Text(
+                        '단식 종료',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w600),
+                      )
+                    : const Text(
+                        '단식 시작',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w600),
+                      ),
               ),
-            ],
+            ),
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          BackGround(),
+          FloatingActionButton(onPressed: () {
+            setState(() {
+              _progress += 1;
+              if (_progress > 57600) {
+                _progress = 0;
+              }
+            });
+          }),
+          const BackGround(),
         ],
       ),
     );
@@ -178,7 +138,7 @@ class _BackGroundState extends State<BackGround>
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 4),
+      duration: const Duration(seconds: 4),
     );
     _topAlignmentAnimation = TweenSequence<Alignment>([
       TweenSequenceItem(
@@ -231,7 +191,7 @@ class _BackGroundState extends State<BackGround>
               height: 250,
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Color(0xff8a55e1), Color(0xfff4c6ca)],
+                  colors: [const Color(0xff8a55e1), const Color(0xfff4c6ca)],
                   begin: _topAlignmentAnimation.value,
                   end: _bottomALignmentAnimaition.value,
                 ),
@@ -256,7 +216,7 @@ class _painter extends CustomPainter {
     double periodParameter = 0.5;
 
     Paint paint = Paint()
-      ..color = Color(0xfffffbff)
+      ..color = const Color(0xfffffbff)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2;
 
@@ -279,4 +239,118 @@ class _painter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class CircularProgress extends StatefulWidget {
+  final double progress;
+  const CircularProgress({super.key, required this.progress});
+
+  @override
+  State<CircularProgress> createState() => _CircularProgressState();
+}
+
+class _CircularProgressState extends State<CircularProgress>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  Animation<double>? _progressTweenAnimation;
+  final double _circularProgressSize = 0.3;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
+
+    _progressTweenAnimation = Tween<double>(begin: 0, end: widget.progress)
+        .animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _animationController.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant CircularProgress oldWidget) {
+    _animationController.reset();
+    _progressTweenAnimation =
+        Tween<double>(begin: oldWidget.progress, end: widget.progress)
+            .animate(_animationController)
+          ..addListener(() {});
+
+    _animationController.forward();
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return CustomPaint(
+      painter: CircularProgressPainter(
+          circularProgressSize: size.width * _circularProgressSize,
+          progress: _progressTweenAnimation?.value ?? 0),
+      child: Container(
+        width: size.width * _circularProgressSize,
+        height: size.width * _circularProgressSize,
+        child: Center(
+          child: Text(
+              "${(_progressTweenAnimation?.value ?? 0).toStringAsFixed(0)}%"),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
+
+class CircularProgressPainter extends CustomPainter {
+  final double circularProgressSize;
+  final double progress;
+  final onePercentageToRadian = 0.06283;
+
+  CircularProgressPainter(
+      {required this.circularProgressSize, required this.progress});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paintBackground = Paint()
+      ..strokeWidth = 16
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    final paintProgress = Paint()
+      ..strokeWidth = 12
+      ..color = Colors.green
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(
+      Offset(circularProgressSize / 2, circularProgressSize / 2),
+      circularProgressSize / 2,
+      paintBackground,
+    );
+
+    canvas.drawArc(
+        Rect.fromCenter(
+            center: Offset(circularProgressSize / 2, circularProgressSize / 2),
+            width: circularProgressSize,
+            height: circularProgressSize),
+        3 * pi / 2,
+        _convertPercentageToRadian(),
+        false,
+        paintProgress);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+  double _convertPercentageToRadian() {
+    return onePercentageToRadian * progress * 100;
+  }
 }
